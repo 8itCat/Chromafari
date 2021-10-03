01001926614c995f5ad38bd1d2462040876537c3
8-Bit Cat <79278890+8itCat@users.noreply.github.com>
2021-10-03 14:59:46 -0400

    Update to include working version

---
+++ SponsorBlock for YouTube/Source Code/macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by 8itCat on 10/3/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
